4d608a74b7da268e230d11c252601c2b82d51f3d
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-24 11:06:32 -0300

    Currency convertor basic style

--- src/CurrencyConvertor/index.js
+++ src/CurrencyConvertor/index.js
@@ -22,11 +22,47 @@ function Convertor() {
     if (data.amount === isNaN) {
       return;
     } else {
-      getCurrencyConvertor();
+      //getCurrencyConvertor();
     }
   }, []);
 
-  return <div className="convertor">index</div>;
+  return (
+    <div className="convertor">
+      <div className="convertor__header">
+        <p1>Taxa de Câmbio</p1>
+        <h1>$27.77</h1>
+      </div>
+
+      <div className="convertor__body">
+        <form className="convertor__body__form">
+          <label for="valor">Valor</label>
+          <input type="number" id="valor" name="valor" />
+
+          <div className="convertor__body__form__currencies">
+            <label for="moeda1">De</label>
+            <select id="moeda1" name="moeda1">
+              <option value="USD">USD</option>
+              <option value="BRL">BRL</option>
+              <option value="EUR">EUR</option>
+            </select>
+
+            <i className="fa-solid fa-arrow-right-arrow-left"></i>
+
+            <label for="moeda2">Para</label>
+            <select id="moeda2" name="moeda2">
+              <option value="USD">USD</option>
+              <option value="BRL" selected>
+                BRL
+              </option>
+              <option value="EUR">EUR</option>
+            </select>
+
+            {/*  */}
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Convertor;
